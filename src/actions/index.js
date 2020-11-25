c34929faec23aae1c158d24860bcf19e0907160f
eldenhor <eldenhorehr@gmail.com>
2020-11-25 22:25:17 +0300

    :sparkles: adding items to cart

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,40 @@
+import bookstoreService from "../services/bookstore-service";
+
 const booksLoaded = (newBooks) => {
   return {
-    type: "BOOKS_LOADED",
+    type: "FETCH_BOOKS_SUCCESS",
     payload: newBooks
   };
 };
 
+const booksRequested = () => {
+  return {
+    type: "FETCH_BOOKS_REQUEST"
+  };
+};
+
+const booksError = (error) => {
+  return {
+    type: "FETCH_BOOKS_FAILURE",
+    payload: error
+  };
+};
+
+const bookAddedToCart = (bookId) => {
+  return {
+    type: "BOOK_ADDED_TO_CART",
+    payload: bookId
+  };
+};
+
+const fetchBooks = (bookstoreService, dispatch) => () => {
+  dispatch(booksRequested());
+  bookstoreService.getBooks()
+    .then((data) => dispatch(booksLoaded(data)))
+    .catch((err) => dispatch(booksError(err)));
+};
+
 export {
-  booksLoaded
+  bookAddedToCart,
+  fetchBooks
 };
